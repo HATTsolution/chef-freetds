3df2ac1817c4b0815bb83ff355f56ef9fbf7226a
Clemens Müller <cmueller.418@gmail.com>
2014-05-08 11:30:53 +0200

    Bump version to 0.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "iain@finalist.nl"
 license           "MIT"
 description       "Installs FreeTDS 0.91"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.2"
+version           "0.0.3"
 
 
 depends "build-essential"
